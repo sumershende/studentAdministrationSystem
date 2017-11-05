79036686a13304e04ebce3d471a05b06447134f6
Sumer <sshende@ncsu.edu>
2017-11-04 20:20:20 -0400

    Populated DB

--- Milestone-2/Code/DBMS_P_1/src/PushDataIntoDb.java
+++ Milestone-2/Code/DBMS_P_1/src/PushDataIntoDb.java
@@ -8,6 +8,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
+import java.util.concurrent.TimeUnit;
 
 public class PushDataIntoDb {
 
@@ -26,6 +27,16 @@ public class PushDataIntoDb {
 			insertHasTa2("HasTa","HasTa",con);	//insert HasTA
 			insertHasTa("Topics","Topics",con);	//insert topics
 			insertHasTa2("Enrolled_In","Enrolled_In",con);	//insert topics
+			insertQuestions("Questions","Questions",con);
+			insertExercises("Exercises","Exercises",con);
+			insertQuestion_In_Ex("Questions_In_Ex","Questions_In_Ex",con); //insert questionsInEx
+			insertParam_Questions("Param_Questions","Param_Questions",con);	//insert param_questions
+			insertParam_Inc_Questions("Param_Inc_Questions","Param_Inc_Questions",con); //insert Param_inc_questions
+			insertParam_Inc_Questions("Param_Answers","Param_Answers",con);	//insert param_answers
+			insertFixedQuestions("Fixed_Questions","Fixed_Questions",con); // Insert fixed_questions
+			insertFixedQuestions("Fixed_Inc_Answers","Fixed_Inc_Answers",con); // Insert Incorrect answers for fixed questions
+			insertFixedQuestions("Fixed_Inc_Answers2","Fixed_Inc_Answers",con); // Insert Incorrect answers for fixed questions2
+			insertFixedQuestions("Fixed_Inc_Answers3","Fixed_Inc_Answers",con); // Insert Incorrect answers for fixed questions3
 			con.close();  
 		  
 		}catch(Exception e){ 
@@ -238,7 +249,6 @@ public class PushDataIntoDb {
         }
 
     }
-    
     static void insertUsers(String filename, String tablename, Connection con){
 
         String csvFile = "C:/Users/Sumer/Desktop/My Files/NCSU Assignments/DBMS/Project 1/Project/Data/"+filename+".csv";
@@ -280,5 +290,254 @@ public class PushDataIntoDb {
         }
 
     }
-    
+    static void insertQuestions(String filename, String tablename, Connection con){
+
+        String csvFile = "C:/Users/Sumer/Desktop/My Files/NCSU Assignments/DBMS/Project 1/Project/Data/"+filename+".csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        boolean first = true;
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+            	if(first) {
+            		first=false;
+            		continue;
+            	}
+                // use comma as separator
+                String[] data = line.split(cvsSplitBy);
+                Statement stmt=con.createStatement();  
+    			String values=data[0]+","+data[1]+",'"+data[2]+"','"+data[3]+"','"+data[4]+"',"+data[5];
+    			System.out.println(values);
+    			stmt.executeQuery("INSERT INTO "+tablename+" VALUES("+values+")");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch(Exception e){ 
+			System.out.println("SQL Error\n"+e);
+		}finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+    static void insertExercises(String filename, String tablename, Connection con){
+
+        String csvFile = "C:/Users/Sumer/Desktop/My Files/NCSU Assignments/DBMS/Project 1/Project/Data/"+filename+".csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        boolean first = true;
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+            	if(first) {
+            		first=false;
+            		continue;
+            	}
+                // use comma as separator
+                String[] data = line.split(cvsSplitBy);
+                Statement stmt=con.createStatement();  
+    			String values=data[0]+",'"+data[1]+"','"+data[2]+"',TO_DATE('"+data[3]+"','MM-DD-YYYY'),"+"TO_DATE('"+data[4]+"','MM-DD-YYYY'),"+data[5]+","+data[6]+",'"+data[7]+"',"+data[8]+","+data[9]+","+data[10];
+    			System.out.println(values);
+    			stmt.executeQuery("INSERT INTO "+tablename+" VALUES("+values+")");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch(Exception e){ 
+			System.out.println("SQL Error\n"+e);
+		}finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+    static void insertQuestion_In_Ex(String filename, String tablename, Connection con){
+
+        String csvFile = "C:/Users/Sumer/Desktop/My Files/NCSU Assignments/DBMS/Project 1/Project/Data/"+filename+".csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        boolean first = true;
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+            	if(first) {
+            		first=false;
+            		continue;
+            	}
+                // use comma as separator
+                String[] data = line.split(cvsSplitBy);
+                Statement stmt=con.createStatement();  
+    			String values=data[0]+","+data[1];
+    			System.out.println(values);
+    			stmt.executeQuery("INSERT INTO "+tablename+" VALUES("+values+")");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch(Exception e){ 
+			System.out.println("SQL Error\n"+e);
+		}finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+    static void insertFixedQuestions(String filename, String tablename, Connection con){
+
+        String csvFile = "C:/Users/Sumer/Desktop/My Files/NCSU Assignments/DBMS/Project 1/Project/Data/"+filename+".csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        boolean first = true;
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+            	
+            	if(first) {
+            		first=false;
+            		continue;
+            	}
+                // use comma as separator
+                String[] data = line.split(cvsSplitBy);
+                Statement stmt=con.createStatement();  
+    			String values=data[0]+",'"+data[1]+"'";
+    			System.out.println(values);
+    			stmt.executeQuery("INSERT INTO "+tablename+" VALUES("+values+")");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch(Exception e){ 
+			System.out.println("SQL Error\n"+e);
+		}finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+    static void insertParam_Questions(String filename, String tablename, Connection con){
+
+        String csvFile = "C:/Users/Sumer/Desktop/My Files/NCSU Assignments/DBMS/Project 1/Project/Data/"+filename+".csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        boolean first = true;
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+            	
+            	if(first) {
+            		first=false;
+            		continue;
+            	}
+                // use comma as separator
+                String[] data = line.split(cvsSplitBy);
+                Statement stmt=con.createStatement();  
+    			String values=data[0]+","+data[1]+","+data[2]+",'"+data[3]+"'";
+    			System.out.println(values);
+    			stmt.executeQuery("INSERT INTO "+tablename+" VALUES("+values+")");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch(Exception e){ 
+			System.out.println("SQL Error\n"+e);
+		}finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+    static void insertParam_Inc_Questions(String filename, String tablename, Connection con){
+
+        String csvFile = "C:/Users/Sumer/Desktop/My Files/NCSU Assignments/DBMS/Project 1/Project/Data/"+filename+".csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        boolean first = true;
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+            	
+            	if(first) {
+            		first=false;
+            		continue;
+            	}
+                // use comma as separator
+                String[] data = line.split(cvsSplitBy);
+                Statement stmt=con.createStatement();  
+    			String values=data[0]+","+data[1]+",'"+data[2]+"'";
+    			System.out.println(values);
+    			stmt.executeQuery("INSERT INTO "+tablename+" VALUES("+values+")");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch(Exception e){ 
+			System.out.println("SQL Error\n"+e);
+		}finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+     
 }
