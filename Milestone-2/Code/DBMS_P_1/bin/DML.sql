b1ed20e92d2e731e49d9b6a9658a6f087d303076
Sumer <sshende@ncsu.edu>
2017-11-05 13:20:11 -0500

    Fix .classpath

--- Milestone-2/Code/DBMS_P_1/bin/DML.sql
+++ Milestone-2/Code/DBMS_P_1/bin/DML.sql
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 select * from courses;
-=======
-select * from Exercises;
->>>>>>> ef1296ceca23b4fcd0430dbf6f69518fd56e53db
--- Milestone-2/Code/DBMS_P_1/src/DBHandler.java
+++ Milestone-2/Code/DBMS_P_1/src/DBHandler.java
@@ -3,7 +3,6 @@ import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
