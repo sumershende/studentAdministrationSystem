dd88fa3dab3ba9a91452f8da3f1fe030063fb29c
Gautam Verma <gautam94verma@gmail.com>
2017-11-05 22:35:32 -0500

    Done from GV

--- Milestone-2/Code/DBMS_P_N_1/src/DBHandler.java
+++ Milestone-2/Code/DBMS_P_N_1/src/DBHandler.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Types;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
